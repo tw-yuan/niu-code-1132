67c6ad712082e6c7d330b5867bbeedc198296c7b
Yuan <me@yuan-tw.net>
2025-02-21 08:50:29 +0000

    更新 h1.cpp，實作簡單的加減乘除運算功能

--- h1.cpp
+++ h1.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
-    cout << "Hello World!" << endl;
+    int a, b;
+    cout << "please input int 1:" << endl;
+    cin >> a;
+    cout << "please input int 2:" << endl;
+    cin >> b;
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " % " << b << " = " << a % b << endl;
     return 0;
 }
