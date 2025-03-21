b8be1dbfb83c61eaff0da3ecec04dd9f5313843c
Yuan <me@yuan-tw.net>
2025-03-21 09:52:57 +0000

    更新貓咪計算功能，簡化程式碼並修正輸入處理邏輯

--- b1343013_w5_3.cpp
+++ b1343013_w5_3.cpp
@@ -1,21 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int cat(int h, int t) {
-    if (h/t == 0) return 0;
-    return h/t + cat(h, t+1);
+int cat(int h) {
+    if (h < 1) return 0;
+    int sum = h;
+    return cat(h / 2) + cat(h / 3) + cat(h / 4) + sum;
 }
 int main() {
-    int h, sum;
+    int h;
     while(true) {
         cout << "Input h:" << endl;
         cin >> h;
         if (h <= 0) break;
-        for (int i = 1; i <= h; i++) {
-            cout << "h=" << h << ", i=" << i << ", h/i=" << h/i << ", cat(h/i, 1)=" << cat(h/i, 1) << endl;
-            sum = sum + cat(h/i, 1);
-        }
-        cout << "Output:" << sum << endl;
-        sum = 0;
+        cout << "Output:" << cat(h) << endl;
     }
     return 0;
 }
